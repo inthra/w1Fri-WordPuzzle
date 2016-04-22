fa5be5742f30774c2cff200e89a0b22678e58eec
Inthrayuth Mahaphol <zign.int@gmail.com>
2016-04-22 09:38:04 -0700

    added methods to replace Uppercase and Lowercase Vowels with '-'. Passed all tests.

--- src/main/java/Puzzle.java
+++ src/main/java/Puzzle.java
@@ -4,7 +4,17 @@
 public class Puzzle {
 
   public String replaceVowels(String userString) {
-    
-    return userString.replace('e', '-');
+    userString = userString.replace('a', '-');
+    userString = userString.replace('e', '-');
+    userString = userString.replace('i', '-');
+    userString = userString.replace('o', '-');
+    userString = userString.replace('u', '-');
+    userString = userString.replace('A', '-');
+    userString = userString.replace('E', '-');
+    userString = userString.replace('I', '-');
+    userString = userString.replace('O', '-');
+    userString = userString.replace('U', '-');
+
+    return userString;
   }
 }
--- src/test/java/PuzzleTest.java
+++ src/test/java/PuzzleTest.java
@@ -4,8 +4,19 @@ import static org.junit.Assert.*;
 public class PuzzleTest {
 
   @Test
-  public void runReplaceVowels_replaceVowelsWithDash_WordPuzzle() {
+  public void runReplaceVowels_replace_e_WithDash_returnWordPuzzle() {
     Puzzle testPuzzle = new Puzzle();
-    assertEquals("B-li-v-", testPuzzle.replaceVowels("Believe"));
+    assertEquals("L--k", testPuzzle.replaceVowels("Leek"));
   }
+  @Test
+  public void runReplaceVowels_replaceVowelsWithDash_returnWordPuzzle() {
+    Puzzle testPuzzle = new Puzzle();
+    assertEquals("C--l-fl-w-r", testPuzzle.replaceVowels("Cauliflower"));
+  }
+  @Test
+  public void runReplaceVowels_replaceUpperCaseVowelsWithDash_returnWordPuzzle() {
+    Puzzle testPuzzle = new Puzzle();
+    assertEquals("-M-R-LD", testPuzzle.replaceVowels("EMERALD"));
+  }
+  
 }
