2913344f0befa86a4c36489abe195ad1b3a2cb7c
Inthrayuth Mahaphol <zign.int@gmail.com>
2016-04-22 12:19:17 -0700

    add app.css to style input and game interface

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,13 @@
 import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
-import static spark.Spark.*;
 import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
 
 public class App {
 
   public static void main(String[] args) {
+    staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
     get("/", (request, response) -> {
